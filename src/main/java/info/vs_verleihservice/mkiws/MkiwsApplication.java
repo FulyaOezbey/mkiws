e53f995bcf6edc5297e9f0c87dc6be5b4bd024a8
FulyaOezbey <fulya.oezbey@g,ail.com>
2020-12-20 15:30:26 +0100

    Start Project

---
+++ src/main/java/info/vs_verleihservice/mkiws/MkiwsApplication.java
@@ -0,0 +1,13 @@
+package info.vs_verleihservice.mkiws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkiwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkiwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/vs_verleihservice/mkiws/MkiwsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.vs_verleihservice.mkiws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkiwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
